393bd901c3d47fc295eccef7a856faf5539e820c
Kendra <kmulligan589@gmail.com>
2021-06-14 14:27:22 -0600

    fixed bug

--- main.js
+++ main.js
@@ -7,11 +7,7 @@
            country: "us"
          }
  };
-const autocomplete = new google.maps.places.Autocomplete(input, options);
-
-
-
- 
+const autocomplete = new google.maps.places.Autocomplete(input, options); 
 
 
 document.querySelector('.autocomplete').addEventListener('keydown', function(e) {
@@ -28,7 +24,7 @@ const split = searchTerm.split(',');
 
 const city = split[0];
 
-    let response = await axios.get('http://api.openweathermap.org/data/2.5/weather?', {
+    let response = await axios.get('https://api.openweathermap.org/data/2.5/weather?', {
         params: {
           q: city,
           appid: '55b1bfcad59eea54e711381bff6e693e',
@@ -48,7 +44,7 @@ function drawWeather(d){
   const farenheit = Math.round((d.main.temp)*1);
   const feelsLike = Math.round((d.main.feels_like)*1);
   const icon = d.weather[0].icon;
-  const iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+  const iconUrl = "https://openweathermap.org/img/w/" + icon + ".png";
 
 
   /*document.getElementById('icon').innerHTML = "<img src='" + iconUrl  + "'>";
